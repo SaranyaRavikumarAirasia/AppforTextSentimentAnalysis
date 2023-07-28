cdd8bcf2a7dab7562a0bf7d5f79e95318f2b89e3
SaranyaRavikumarAirasia <116171141+SaranyaRavikumarAirasia@users.noreply.github.com>
2023-07-28 11:11:30 +0800

    textblobalgorithm partially completed

---
+++ textblobalgthm.py
@@ -0,0 +1,37 @@
+from textblob import TextBlob
+import streamlit as st
+import stremlit.components.v1 as components
+
+#Create user defined function
+def displayPage():
+  st.subheader("Text Analysis using Text Blob")
+  st.text("Enter the user text to be analysed")
+  userText=st.text_input('Input',placeholder='Input the text here')
+  st.text(" ")
+  if st.button('Predict'):
+    if(userText!=""):
+      st.components.v1.html("""<h3 style="color:#0284c7;font-family:Source Sans Pro,sans-serif;font-size:28px;margin-bottom:8px;margin-top:55px;">RESULT</h3>""",height=150)
+      getSentiment(userText)
+
+
+# Write the user defined function getSentiment
+
+def getSentiment(userText):
+  polarity,subj,status=getPolarity(userText)
+  if(status=="Positive"):
+    image
+
+
+
+
+
+def getPolarity(userText):
+  tb=TextBlob(userText)
+  polarity=round(tb.polarity,2)
+  subj=round(tb.subjectivity,2)
+  if polarity>0:
+    return polarity,subj,"Positive"
+  elif polarity==0:
+    return polarity,subj,"Neutral"
+  else:
+    return polarity,subj,"Negative"
