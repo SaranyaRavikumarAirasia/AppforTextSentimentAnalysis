b3692d87a8869e4a908d496fdb972aca82098641
SaranyaRavikumarAirasia <116171141+SaranyaRavikumarAirasia@users.noreply.github.com>
2023-07-28 13:07:07 +0800

    Update mainbarpage.py

--- mainbarpage.py
+++ mainbarpage.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import sidebarpage
 import textblobalgthm
-import texttoemotion
+import tem
 import vaderalgthm
 page=sidebarpage.show()
 if page=="Text2Emotion Analysis":
-  texttoemotion.displayPage()
+  tem.displayPage()
 elif page=="TextBlob Analysis":
   textblobalgthm.displayPage()
 elif page=="VADER Sentiment Analysis":
