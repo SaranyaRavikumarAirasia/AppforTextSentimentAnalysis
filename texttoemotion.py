1fa9d60fa5167244cdae08aa36da16de1ce92f6f
SaranyaRavikumarAirasia <116171141+SaranyaRavikumarAirasia@users.noreply.github.com>
2023-07-28 12:19:25 +0800

    Update texttoemotion.py

--- texttoemotion.py
+++ texttoemotion.py
@@ -11,4 +11,22 @@ def displayPage():
   st.text("")
   if st.button("Predict"):
     if (userText!=""):
-      
+      st.components.v1.html("""<h3 style="color:#0284c7;font-family:Source Sans Pro,sans-serif;font-size:28px;margin-bottom:8px;margin-top:55px;">RESULT</h3>""",height=150)
+      getSentiment(userText)
+
+def getSentiment(userText):
+  emotion1=dict(te.get_emotion(userText))
+  col1,col2,col3,col4,col5=st.columns(5)
+  col1.metric("Happy",emotion1['Happy'],None)
+  col2.metric("Sad",emotion1['Sad'],None)
+  col3.metric("Angry",emotion1['Angry'],None)
+  col4.metric("Fear",emotion1['Fear'],None)
+  col5.metric("Surprise",emotion1['Surprise'],None)
+  print(emotion1)
+
+def plotBar(labels,values):
+  f=go.figure(
+    go.bar(labels=labels,values=values,textinfo="value"
+          ))
+  st.plotly_chart(f)
+  
