79bc1315844a4bd9b23ff8ab1a37d7b5ac029340
SaranyaRavikumarAirasia <116171141+SaranyaRavikumarAirasia@users.noreply.github.com>
2023-07-28 12:27:12 +0800

    Update mainbarpage.py

--- mainbarpage.py
+++ mainbarpage.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import sidebarpage
 import textblobalgthm
-import texttoemot
+import texttoemotion
 page=sidebarpage.show()
 if(page=="TextBlob Analysis"):
   textblobalgthm.displayPage()
 elif(page=="Text2Emotion Analysis"):
-  texttoemot.displayPage()
+  texttoemotion.displayPage()
