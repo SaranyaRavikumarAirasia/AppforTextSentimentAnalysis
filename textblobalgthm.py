525ec08a86e075b53893c0aaaa61f0895eb6fa34
SaranyaRavikumarAirasia <116171141+SaranyaRavikumarAirasia@users.noreply.github.com>
2023-07-28 11:22:39 +0800

    Update textblobalgthm.py

--- textblobalgthm.py
+++ textblobalgthm.py
@@ -1,6 +1,7 @@
 from textblob import TextBlob
 import streamlit as st
 import stremlit.components.v1 as components
+from PIP import Image
 
 #Create user defined function
 def displayPage():
@@ -13,17 +14,16 @@ def displayPage():
       st.components.v1.html("""<h3 style="color:#0284c7;font-family:Source Sans Pro,sans-serif;font-size:28px;margin-bottom:8px;margin-top:55px;">RESULT</h3>""",height=150)
       getSentiment(userText)
 
-
 # Write the user defined function getSentiment
 
 def getSentiment(userText):
   polarity,subj,status=getPolarity(userText)
   if(status=="Positive"):
-    image
-
-
-
-
+    image=Image.open('./images/positive.png')
+  elif(status=="Negative"):
+    image=Image.open('./images/negative.png')
+  else:
+    image=Image.open('./images/neutral.png')
 
 def getPolarity(userText):
   tb=TextBlob(userText)
