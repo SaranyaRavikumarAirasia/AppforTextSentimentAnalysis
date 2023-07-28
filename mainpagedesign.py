6ee0c8dfb36df7ec7723ffcff4b7b45638574b26
SaranyaRavikumarAirasia <116171141+SaranyaRavikumarAirasia@users.noreply.github.com>
2023-07-28 10:52:42 +0800

    Create mainpagedesign.py

---
+++ mainpagedesign.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import sidebarpage
+import textblobalgthm
+import 
+page=sidebarpage.show()
+if page=="TextBlob Analysis":
+  textblobalgthm.renderPage()
+elif page=="Text2Emotion Algorithm"
+  
+  
