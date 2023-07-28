556fecc73793e4671b71fb42a6fed714c3ae6bc5
SaranyaRavikumarAirasia <116171141+SaranyaRavikumarAirasia@users.noreply.github.com>
2023-07-28 11:29:56 +0800

    Create sidebarpage.py

---
+++ sidebarpage.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+def show():
+    bg = """<div style='background-color:black; padding:13px'>
+              <h1 style='color:white'>Sentiment Analysis  😊😐😕😡</h1>
+       </div>"""
+    st.markdown(bg, unsafe_allow_html=True)
+    with st.sidebar:
+      selectedoption=option_menu(
+        menu_title = "👇Select the type of Machine Learning for Text Sentiment Analysis",
+        options = ["TextBlob Analysis", "Text2Emotion Analysis","VADER Sentiment Analysis"], #required
+        default_index=0,)
+      return selectedoption
