27c8df8ad91def87f3d5d920fdf1b0c65a97c5ba
SaranyaRavikumarAirasia <116171141+SaranyaRavikumarAirasia@users.noreply.github.com>
2023-07-28 12:58:33 +0800

    Update mainbarpage.py

--- mainbarpage.py
+++ mainbarpage.py
@@ -4,9 +4,9 @@ import textblobalgthm
 import texttoemotion
 import vaderalgthm
 page=sidebarpage.show()
-if page=="TextBlob Analysis":
-  textblobalgthm.displayPage()
-elif page=="Text2Emotion Analysis":
+if page=="Text2Emotion Analysis":
   texttoemotion.displayPage()
+elif page=="TextBlob Analysis":
+  textblobalgthm.displayPage()
 elif page=="VADER Sentiment Analysis":
   vaderalgthm.displayPage()
